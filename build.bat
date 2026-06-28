dec2dc4c8ceda40d49ec424d4ec69197637c4090
Yousv <68682169+yousv@users.noreply.github.com>
2026-06-28 10:01:45 +0300

    Fix formatting in build.bat script

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 rc launcher.rc
 cl launcher.c launcher.res "/Fe:SMAPI SilentLauncher.exe" /link user32.lib gdi32.lib /subsystem:windows
-echo Done. SMAPISilentLauncher.exe is ready.
+echo Done. SMAPI SilentLauncher.exe is ready.
 pause
