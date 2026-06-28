444540a2568b5c602e13afafef4731e1d1a3513a
Yousv <68682169+yousv@users.noreply.github.com>
2026-06-28 09:41:37 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 rc launcher.rc
-cl launcher.c launcher.res /Fe:"SMAPI SilentLauncher.exe" /link user32.lib gdi32.lib /subsystem:windows
-echo Done. SMAPI SilentLauncher.exe is ready.
+cl launcher.c launcher.res /Fe:"SMAPISilentLauncher.exe" /link user32.lib gdi32.lib /subsystem:windows
+echo Done. SMAPISilentLauncher.exe is ready.
 pause
